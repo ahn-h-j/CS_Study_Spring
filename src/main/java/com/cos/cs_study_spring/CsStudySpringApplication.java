fa17872c718b41cc29c5a30a06bd5ec4f71f231f
ahn-h-j <tkgkd246@gmail.com>
2026-01-01 19:23:57 +0900

    git init

---
+++ src/main/java/com/cos/cs_study_spring/CsStudySpringApplication.java
@@ -0,0 +1,13 @@
+package com.cos.cs_study_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsStudySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsStudySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cos/cs_study_spring/CsStudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.cs_study_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsStudySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
